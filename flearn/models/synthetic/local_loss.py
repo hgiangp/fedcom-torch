9ad713dfd71b220d9cc63dd7d0b503dab710d1ab
giang <phamhuonggiang.hust@gmail.com>
2023-01-30 20:59:09 +0900

    update model set_params(): v1

--- flearn/models/synthetic/local_loss.py
+++ flearn/models/synthetic/local_loss.py
@@ -14,7 +14,7 @@ class LinearRegression(nn.Module):
 
 model = LinearRegression()
 optimizer = optim.SGD(model.parameters(), lr=1e-2)
-criterion = nn.MSELoss()
+loss_fn = nn.MSELoss()
 
 x = torch.randn(4, 2)
 y = torch.randn(4, 3)
@@ -31,29 +31,74 @@ def serialize_params(model):
 def serialize_gards(model): 
     return {k: v.grad for k, v in zip(model.state_dict(), model.parameters())}
 
-def grads_params_add(grads_dict, params_dict): 
+def add_params_grads(params_dict, grads_dict): 
     sum = 0.0
     for k in params_dict.keys(): 
         sum += (params_dict[k] * grads_dict[k]).sum()
 
-    return sum 
+    return sum
+
+def set_params(model_params=None):
+    r"""Set initial params to model parameters()
+    Args: 
+        model_params: dict {'state_dict': model.parameters().data}
+    Return: None 
+        set model.parameters = model_params 
+    """
+    with torch.no_grad(): 
+        for name, param in zip(model.state_dict(), model.parameters()): 
+            print(model_params[name])
+            param.data = model_params[name] 
+    print(serialize_params(model))
+
+def train(): 
+    for epoch in range(20): 
+        optimizer.zero_grad()
+        output = model(x)
+
+        loss = loss_fn(output, y)
+        surr_term = add_params_grads(serialize_params(model), diff_dict)
+        loss = loss + surr_term
+        
+        loss.backward()
+        optimizer.step()
+        print('Epoch {}: loss {}'.format(epoch, loss.item()))
+    # https://discuss.pytorch.org/t/how-to-add-a-loss-term-from-hidden-layers/47804
 
 # grads_params_add(diff_dict, serialize_params(model))
 # https://discuss.pytorch.org/t/how-to-add-a-loss-term-from-hidden-layers/47804
-for epoch in range(100): 
-    optimizer.zero_grad()
-    output = model(x)
 
-    loss = criterion(output, y)
-    surr_term = grads_params_add(diff_dict, serialize_params(model))
-    loss = loss + surr_term
+
+
+def test_set_params(): 
+    print('test_set_params(): start')
+    # first train 
+    train()
+    print(serialize_params(model))
+
+    # save optimal param to dict 
+    pretrained_params_dict = serialize_params(model)
+
+    # init test_params copy dictionary 
+    test_params_dict = {k: torch.zeros_like(v.data) for k, v in zip(model.state_dict(), model.parameters())}
+    print(f'test_params_dict dtype = {type(test_params_dict)}')
+    print(test_params_dict)
+
+    # set all model.parameters() to zeros 
+    print('='*20)
+    set_params(test_params_dict)
+    print('='*20)
+    print(serialize_params(model)) 
+
+    # load the pretrained params to model.parameters 
+    set_params(pretrained_params_dict)
+
+    # continue training 
+    train()
+
+    print(serialize_params(model)) 
     
-    loss.backward()
-    optimizer.step()
-    print('Epoch {}: loss {}'.format(epoch, loss.item()))
-# https://discuss.pytorch.org/t/how-to-add-a-loss-term-from-hidden-layers/47804
+    print('test_set_params(): done')
 
-print(serialize_params(model))
-
-
-
+if __name__=='__main__': 
+    test_set_params()
