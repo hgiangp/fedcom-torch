09e2c26886e5dbc1e77ab50c7eef5c98f0d4baf5
giang <phamhuonggiang.hust@gmail.com>
2023-03-22 13:42:07 +0900

    update fake log

--- examples/network_params.py
+++ examples/network_params.py
@@ -18,7 +18,7 @@ freq_max = 2 * 1e9 # 2GHz -> Hz, maximum cpu computation frequency
 xi_factor = 0.1 # global gradient factor 
 epsilon_0 = 1e-3 # global accuracy 
 compression_ratio = 0.05 # 1-0.95
-s_n = 502400 # 502400*compression_ratio=25120 bits # data transmission size TODO (784 * 10 + 10) * 2 * 32 = 502400 bits 
+s_n = 25120 # 502400*compression_ratio=25120 bits # data transmission size TODO (784 * 10 + 10) * 2 * 32 = 502400 bits 
 bw = 1e6 # 1MHz bandwidth 
 delta_t = 0.0 # TODO Based on current results uav's time penalty 
 N_0 = 4*1e-12 # -174 dBm?/Hz, noise density, should be multiplied with bw: 1e(-17.4)*1e6 = 4*1e(-12) (4*1e-15?)
