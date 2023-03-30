ce23102e9a3505eb735d4be17736006070dafe81
giang <phamhuonggiang.hust@gmail.com>
2023-03-30 10:21:29 +0900

    update synthetic dataset num_users=10

--- data/synthetic/generate_synthetic.py
+++ data/synthetic/generate_synthetic.py
@@ -10,9 +10,9 @@ import json
 # seed = 42
 rng = np.random.default_rng()
 
-NUM_USERS = 5 # TODO
+num_users = 10 # TODO
 dimension = 5 # TODO: 60 
-NUM_CLASSES = 3 # TODO: 10 
+num_classes = 3 # TODO: 10 
 
 def softmax(x): 
     ex = np.exp(x)
@@ -20,13 +20,13 @@ def softmax(x):
     return ex/sum_ex
 
 def generate_synthetic(alpha, beta, iid): 
-    samples_per_user = rng.lognormal(mean=4, sigma=2, size=NUM_USERS).astype(int) +  100# shape = (NUM_USERS,)
+    samples_per_user = rng.lognormal(mean=4, sigma=2, size=num_users).astype(int) +  100# shape = (NUM_USERS,)
     print(samples_per_user)
 
     ### define variables ### 
-    mean_W = rng.normal(loc=0, scale=alpha, size=NUM_USERS) # (NUM_USERS, )
+    mean_W = rng.normal(loc=0, scale=alpha, size=num_users) # (NUM_USERS, )
     mean_b = mean_W # (NUM_USERS, )
-    B = rng.normal(loc=0, scale=beta, size=(NUM_USERS, 1)) # (NUM_USERS, 1)
+    B = rng.normal(loc=0, scale=beta, size=(num_users, 1)) # (NUM_USERS, 1)
 
     diagonal = np.array([np.power((j+1), -1.2) for j in range(dimension)]) # (dimension, )
     cov_x = np.diag(diagonal) # (dimension, dimension)
@@ -34,22 +34,22 @@ def generate_synthetic(alpha, beta, iid):
     if iid: # all users' features follow the same distribution 
         mean_x = np.tile(B, reps=dimension)
     else: 
-        mean_x = np.array([rng.normal(loc=B[i], scale=1, size=dimension) for i in range(NUM_USERS)]) 
+        mean_x = np.array([rng.normal(loc=B[i], scale=1, size=dimension) for i in range(num_users)]) 
     print("mean_x.shape = {}".format(mean_x.shape)) # (users, dimension)
         
     if iid: # params are generated from the unit distribution mean = 0, deviation = 1
-        W_global = rng.normal(loc=0, scale=1, size=(dimension, NUM_CLASSES))
-        b_global = rng.normal(loc=0, scale=1, size=NUM_CLASSES)
+        W_global = rng.normal(loc=0, scale=1, size=(dimension, num_classes))
+        b_global = rng.normal(loc=0, scale=1, size=num_classes)
     
-    X_split = [[] for _ in range(NUM_USERS)]
-    y_split = [[] for _ in range(NUM_USERS)]
-    for i in range(NUM_USERS): 
+    X_split = [[] for _ in range(num_users)]
+    y_split = [[] for _ in range(num_users)]
+    for i in range(num_users): 
         if iid: # parameters are identical for all users 
             W = W_global
             b = b_global
         else: 
-            W = rng.normal(loc=mean_W[i], scale=1, size=(dimension, NUM_CLASSES)) # (dimension, classes)
-            b = rng.normal(loc=mean_b[i], scale=1, size=NUM_CLASSES) # (classes, )
+            W = rng.normal(loc=mean_W[i], scale=1, size=(dimension, num_classes)) # (dimension, classes)
+            b = rng.normal(loc=mean_b[i], scale=1, size=num_classes) # (classes, )
         
         xx = rng.multivariate_normal(mean=mean_x[i], cov=cov_x, size=samples_per_user[i]) # (num_samples_i, dimension) 
         tmp = softmax(np.dot(xx, W) + b) # (samples, classes)
@@ -73,7 +73,7 @@ def main():
     train_data = {'user': [], 'user_data': {}, 'num_samples': []}    
     test_data = {'user': [], 'user_data': {}, 'num_samples': []}
 
-    for i in trange(NUM_USERS, ncols=120):
+    for i in trange(num_users, ncols=120):
         uname = 'f_{0:05d}'.format(i)
         combined = list(zip(X[i], y[i]))
         rng.shuffle(combined)
