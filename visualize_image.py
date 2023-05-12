f7266485f9cf4891756f0f8324d1d57312138022
giang <phamhuonggiang.hust@gmail.com>
2023-05-12 21:31:33 +0900

    update tSNE plot

--- visualize_image.py
+++ visualize_image.py
@@ -4,8 +4,37 @@ import os
 import importlib
 import torch
 from tqdm import trange
+from sklearn.manifold import TSNE
+import pandas as pd 
+import seaborn as sn 
 
 from src.custom_dataset import * 
+
+def load_model(train_data: dict):
+    options={'dataset': 'mnist', 'sce_idx': 4, 'model': 'mclr', 'model_params': (784, 10)}
+        
+    # load selected model 
+    # init the saved model 
+    model_path = '%s.%s.%s.%s' % ('flearn', 'models', options['dataset'], options['model'])
+    model_lib = importlib.import_module(model_path)
+    model_mclr = getattr(model_lib, 'Model')(options['model_params'])
+
+    # saved model direction 
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    save_dir = os.path.join(parent_dir, 'models', options['dataset'], f's{str(options["sce_idx"])}')
+    model_path = save_dir + '/'+ 'model_weights.pth' 
+
+    # load the saved model
+    model_mclr.model.load_state_dict(torch.load(model_path))
+    print('Model loaded!')
+
+    training_data =  CustomDataset(train_data)
+    train_dataloader = DataLoader(training_data, batch_size=32, shuffle=False, drop_last=False)
+
+    # evaluate on the train set
+    y_pred = model_mclr.predict(train_dataloader, debug=False)
+
+    return y_pred
 
 def view_data(): 
     num_users = 10
@@ -37,32 +66,55 @@ def view_data():
         plt.savefig(f'./figures/mnist/images/image_user{user_id}.png')
         plt.show()
 
-def load_model(train_data: dict):
-    options={'dataset': 'mnist', 'sce_idx': 4, 'model': 'mclr', 'model_params': (784, 10)}
+def view_SNE():
+    # configure TSNE model 
+    # the number of components = 2
+    # default perplexity = 30
+    # default learning rate = 200
+    # default Maximum number of iterations for the optimization = 1000
+    model = TSNE(n_components=2, random_state=0)
+
+    # load data 
+    num_users = 10
+
+    test_data, _ = test_load_data(user_id=0, dataset_name='mnist')
+    y_pred = load_model(test_data)
+    X, y = test_data['x'], test_data['y']
+    X_reshape = np.array([np.asarray(x) for x in X])
+    y_pred = np.asarray(y_pred)
+
+    X_all = X_reshape
+    y_all = y 
+
+    for user in range(1, num_users, 1): 
+        print(user)
+        test_data, _ = test_load_data(user_id=user, dataset_name='mnist')
+        y_pred = load_model(test_data)
+        X, y = test_data['x'], test_data['y']
         
-    # load selected model 
-    # init the saved model 
-    model_path = '%s.%s.%s.%s' % ('flearn', 'models', options['dataset'], options['model'])
-    model_lib = importlib.import_module(model_path)
-    model_mclr = getattr(model_lib, 'Model')(options['model_params'])
+        X_reshape = np.array([np.asarray(x) for x in X])
+        y_pred = np.asarray(y_pred)
 
-    # saved model direction 
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    save_dir = os.path.join(parent_dir, 'models', options['dataset'], f's{str(options["sce_idx"])}')
-    model_path = save_dir + '/'+ 'model_weights.pth' 
+        X_all = np.concatenate((X_all, X_reshape), axis=0)
+        y_all = np.concatenate((y_all, y_pred), axis=0)
+    
+    print(f"X_all.shape = {X_all.shape}")
+    print(f"y_all.shape = {y_all.shape}")
 
-    # load the saved model
-    model_mclr.model.load_state_dict(torch.load(model_path))
-    print('Model loaded!')
+    tsne_data = model.fit_transform(X_all)
 
-    training_data =  CustomDataset(train_data)
-    train_dataloader = DataLoader(training_data, batch_size=32, shuffle=False, drop_last=False)
+    # Creating a new data frame which helps us in ploting the result data
+    tsne_data = np.vstack((tsne_data.T, np.asarray(y_all))).T
+    tsne_df = pd.DataFrame(data=tsne_data, columns=("dim_1", "dim_2", "label"))
 
-    # evaluate on the train set
-    y_pred = model_mclr.predict(train_dataloader, debug=False)
+    # Ploting the result of tsne
+    sn.FacetGrid(tsne_df, hue="label", height=8).map(plt.scatter, 'dim_1', 'dim_2', alpha=0.8).add_legend()
+    #Use size if height results in warning.
 
-    return y_pred
+    plt.savefig(f'./figures/mnist/images/tSNE.png')
+    plt.show()
 
 if __name__=='__main__': 
-    view_data()
+    # view_data()
     # load_model()
+    view_SNE()
