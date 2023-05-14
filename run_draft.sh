811995e94b106a81767d3a394576ba2c07c18013
huong giang <phamhuonggiang.hust@gmail.com>
2023-05-14 12:01:42 +0900

    update lr=0.01 tested

--- run_draft.sh
+++ run_draft.sh
@@ -2,9 +2,9 @@
 
 # python3 ./flearn/models/synthetic/mclr.py| tee logs/mclr.log
 # python3 client_model.py | tee logs/client_model.log
-python3 -u main.py --sce_idx=$1 --tau=$2 --dataset=$3 --model=$4 --learning_rate=$5| tee logs/main.log
-# ./run.sh 40 mnist mclr
-# ./run_main.sh 4 40 mnist mclr
+# python3 -u main.py --sce_idx=$1 --tau=$2 --dataset=$3 --model=$4 --learning_rate=$5| tee logs/main.log
+./run.sh 40 mnist mclr 0.01
+# ./run_main.sh 4 40 mnist mclr 0.01
 # mkdir ./figures/mnist
 # cd ./figures/mnist
 # mkdir s1 s2 s3 s4 
