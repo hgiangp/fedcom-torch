cc4d06d3a2c2e03605f8245459dc56687b4075fc
huong giang <phamhuonggiang.hust@gmail.com>
2023-06-02 11:38:52 +0900

    temporary comparison

--- run_draft.sh
+++ run_draft.sh
@@ -10,8 +10,8 @@
 # python -m torch.utils.bottleneck main.py --sce_idx=$sce_idx --tau=$tau --dataset=$dataset --model=$model --learning_rate=$learning_rate \
     # | tee logs/$dataset/s$sce_idx/system_model.log
 
-./run_main.sh 4 40 mnist mclr 0.01
-# ./run.sh 40 mnist mclr 0.01
+# ./run_main.sh 4 40 mnist mclr 0.01
+./run.sh 40 mnist mclr 0.01
 
 # sce_idx=4
 # tau=40
